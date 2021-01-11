1fb0f61969ab89eff49de0bab8d91976183c9a23
Arjun Vikram <arjvik@gmail.com>
2021-01-11 15:14:53 -0600

    Cache script outputs

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ Taken from https://dagshub.com/docs/experiment-tutorial/2-data-versioning/ with 
 The goal of this repository is not to learn how to create an NLP classifier but to learn how to use DVC, therefore the ML code is unimportant.
 """
 
+import argparse
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, average_precision_score, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
+import joblib
+
+# Consts
+CLASS_LABEL = 'MachineLearning'
+train_df_path = 'data/train.csv.gz'
+test_df_path = 'data/test.csv.gz'
 
 
 def feature_engineering(raw_df):
     df = raw_df.copy()
     df['CreationDate'] = pd.to_datetime(df['CreationDate'])
     df['CreationDate_Epoch'] = df['CreationDate'].astype('int64') // 10 ** 9
-    df['MachineLearning'] = df['Tags'].str.contains('machine-learning').fillna(False)
     df = df.drop(columns=['Id', 'Tags'])
     df['Title_Len'] = df.Title.str.len()
     df['Body_Len'] = df.Body.str.len()
@@ -33,8 +39,8 @@ def fit_tfidf(train_df, test_df):
     return train_tfidf, test_tfidf, tfidf
 
 
-def fit_model(train_X, train_y):
-    clf_tfidf = LogisticRegression(solver='sag')
+def fit_model(train_X, train_y, random_state=42):
+    clf_tfidf = LogisticRegression(solver='sag', random_state=random_state)
     clf_tfidf.fit(train_X, train_y)
     return clf_tfidf
 
@@ -52,26 +58,55 @@ def eval_model(clf, X, y):
     }
 
 
-if __name__ == '__main__':
+def split(random_state=42):
     print('Loading data...')
     df = pd.read_csv('data/CrossValidated-Questions-Nov-2020.csv')
-    train_df, test_df = train_test_split(df)
-    del df
+    df[CLASS_LABEL] = df['Tags'].str.contains('machine-learning').fillna(False)
+    train_df, test_df = train_test_split(df, random_state=random_state, stratify=df[CLASS_LABEL])
 
+    print('Saving split data...')
+    train_df.to_csv(train_df_path)
+    test_df.to_csv(test_df_path)
+
+
+def train():
+    print('Loading data...')
+    train_df = pd.read_csv(train_df_path)
+    test_df = pd.read_csv(test_df_path)
+
+    print('Engineering features...')
     train_df = feature_engineering(train_df)
     test_df = feature_engineering(test_df)
 
     print('Fitting TFIDF...')
     train_tfidf, test_tfidf, tfidf = fit_tfidf(train_df, test_df)
 
-    print('Fitting classifier...')
-    train_y = train_df['MachineLearning']
+    print('Saving TFIDF object...')
+    joblib.dump(tfidf, 'outputs/tfidf.joblib')
+
+    print('Training model...')
+    train_y = train_df[CLASS_LABEL]
     model = fit_model(train_tfidf, train_y)
 
+    print('Saving trained model...')
+    joblib.dump(model, 'outputs/model.joblib')
+
+    print('Evaluating model...')
     train_metrics = eval_model(model, train_tfidf, train_y)
     print('Train metrics:')
     print(train_metrics)
 
-    test_metrics = eval_model(model, test_tfidf, test_df['MachineLearning'])
+    test_metrics = eval_model(model, test_tfidf, test_df[CLASS_LABEL])
     print('Test metrics:')
     print(test_metrics)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='Split or Train step:', dest='step')
+    subparsers.required = True
+    split_parser = subparsers.add_parser('split')
+    split_parser.set_defaults(func=split)
+    train_parser = subparsers.add_parser('train')
+    train_parser.set_defaults(func=train)
+    parser.parse_args().func()
